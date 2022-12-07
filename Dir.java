0495ae88586107f9f6fdbcafe9217f403d98b799
Jason Lin <jlin31@stuy.edu>
2022-12-07 14:03:36 -0500

    Completed

--- Dir.java
+++ Dir.java
@@ -25,65 +25,58 @@ public class Dir {
       ex.printStackTrace();
     }
 
-    int ans2 = 0;
     ArrayList<String> depth = new ArrayList<String>();
-    Map<String, Integer> map = new HashMap<String, Integer>();
+    // Map<String, Integer> map = new HashMap<String, Integer>();
+    ArrayList<Integer> s = new ArrayList<Integer>();
+    ArrayList<Integer> sizes = new ArrayList<Integer>();
 
+    int ans2 = 0;
     for (int i = 0; i < input.size(); i++) {
       String cmd = input.get(i);
       if (cmd.equals("$ cd ..")) {
-        String n = depth.get(depth.size()-1);
-        // System.out.println("Leaving Dir:" + n + " Line: " + i);
-        int c = map.get(n);
+        int l = depth.size()-1;
+        depth.remove(l);
+        int c = s.get(l);
         if (c <= 100000) {
-            ans2 += c;
+          ans2 += c;
         }
-        map.remove(n);
-        // System.out.println(map.get(n));
-        depth.remove(depth.size()-1);
-      }
-      else if (cmd.equals("$ ls")) {
-
+        sizes.add(c);
+        s.remove(l);
       }
       else if (cmd.contains("$ cd ")) {
         cmd = cmd.replace("$ cd ", "");
-        // System.out.println("Entering: " + cmd);
         depth.add(cmd);
+        s.add(0);
       }
       else if (cmd.contains("dir ")) {
 
       }
+      else if (cmd.equals("$ ls")) {
+
+      }
       else {
-        // System.out.println(cmd);
         String[] vals = cmd.split(" ");
         int gigs = Integer.parseInt(vals[0]);
-        // System.out.println("Adding " + gigs + " from " + vals[1] + " to ");
-        // System.out.println(gigs);
-        for (int k = 0; k < depth.size(); k++) {
-          String cur = depth.get(k);
-        // System.out.println("\t" + cur);
-          if (map.get(cur) != null) {
-            map.put(cur, map.get(cur) + gigs);
-          }
-          else {
-            map.put(cur, gigs);
-          }
+        for (int k = 0; k < s.size(); k++) {
+          s.set(k, s.get(k)+gigs);
         }
       }
+      // System.out.println(depth.toString());
+      // System.out.println(s.toString());
+    }
+    for (int i = 0; i < s.size(); i++) {
+      sizes.add(s.get(i));
     }
 
-    Collection<Integer> values = map.values();
-    System.out.println(map.values());
-    // // System.out.println(vals.size() + " " + dirs);
-    // int ans = 0;
-    // for (int s: values) {
-    //   if (s <= 100000) {
-    //     // System.out.println(s);
-    //     ans += s;
-    //   }
-    // }
-    // System.out.println(ans);
-    System.out.println(ans2);
-
+    Collections.sort(sizes);
+    int spaceUsed = s.get(0);
+    int amount = 30000000 - (70000000 - spaceUsed);
+    for (int i = 0; i < sizes.size(); i++) {
+      int n = sizes.get(i);
+      if (n >= amount) {
+        System.out.println(n);
+        break;
+      }
+    }
   }
 }
