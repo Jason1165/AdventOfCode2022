08eebd39191065803d24ac3a4cf141fd34f34b10
Jason Lin <jlin31@stuy.edu>
2022-12-09 13:42:08 -0500

    Cleaned up code

--- 09Day/Part2.java
+++ 09Day/Part2.java
@@ -1,11 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
+  import java.io.File;
+  import java.io.FileNotFoundException;
+  import java.util.Arrays;
+  import java.util.Scanner;
+  import java.util.ArrayList;
+  import java.util.Collections;
 
-public class Part2 {
+  public class Part2 {
   public static void main(String[] args) {
     solve(args[0]);
   }
@@ -34,43 +34,30 @@ public class Part2 {
       String n = lines.get(i);
       String dir = n.substring(0, 1);
       int dist = Integer.parseInt(n.substring(2));
-      // System.out.println(dist);
       for (int j = 0; j < dist; j++) {
         if (dir.equals("U")) {
           snake.get(0).addX(-1);
-          // headX--;
         }
         if (dir.equals("D")) {
           snake.get(0).addX(1);
-          // headX++;
         }
         if (dir.equals("L")) {
           snake.get(0).addY(-1);
-          // headY--;
         }
         if (dir.equals("R")) {
           snake.get(0).addY(1);
-          // headY++;
         }
         for (int k = 1; k < snake.size(); k++) {
-          // System.out.println("Head: " + snake.get(k-1).toString() + " " + k);
-          // System.out.println("Tail: " + snake.get(k).toString() + " " + k);
           if (!snake.get(k).touching(snake.get(k-1))) {
-            // System.out.println("Head: " + snake.get(k-1).toString() + " " + k);
-            // System.out.println("Tail: " + snake.get(k).toString() + " " + k);
             snake.get(k).move(snake.get(k-1));
           }
         }
-        // System.out.println(snake.toString());
         String s = snake.get(snake.size()-1).toString();
-        // System.out.println(s);
         if (!locations.contains(s)) {
           locations.add(s);
         }
-
       }
-      // System.out.println("FINAL: " + snake.toString());
     }
     System.out.println(locations.size());
   }
-}
+  }
--- 09Day/Point.java
+++ 09Day/Point.java
@@ -1,91 +1,80 @@
-import java.util.ArrayList;
+  public class Point {
+    private int x;
+    private int y;
 
-public class Point {
-  private int x;
-  private int y;
+    public Point(int a, int b) {
+      x = a;
+      y = b;
+    }
 
-  public Point(int a, int b) {
-    x = a;
-    y = b;
-  }
+    public Point() {
+      x = 0;
+      y = 0;
+    }
 
-  public Point() {
-    x = 0;
-    y = 0;
-  }
+    public String toString() {
+      String n = "(" + x + ", " + y + ")";
+      return n;
+    }
 
-  public String toString() {
-    String n = "(" + x + ", " + y + ")";
-    return n;
-  }
+    public int getX() {
+      return x;
+    }
 
-  public int getX() {
-    return x;
-  }
+    public int getY() {
+      return y;
+    }
 
-  public int getY() {
-    return y;
-  }
+    public int addX(int n) {
+      x += n;
+      return getX();
+    }
 
-  public int addX(int n) {
-    x += n;
-    return getX();
-  }
+    public int addY(int n) {
+      y += n;
+      return getY();
+    }
 
-  public int addY(int n) {
-    y += n;
-    return getY();
-  }
+    public boolean touching(Point other) {
+      int oX = other.getX();
+      int oY = other.getY();
+      int disX = Math.abs(x - oX);
+      int disY = Math.abs(y - oY);
+      return (disX <= 1 && disY <= 1);
+    }
 
-  public boolean touching(Point other) {
-    int oX = other.getX();
-    int oY = other.getY();
-    int disX = Math.abs(x - oX);
-    int disY = Math.abs(y - oY);
-    return (disX <= 1 && disY <= 1);
-  }
-
-  public void move(Point p) {
-      int headX = p.getX();
-      int headY = p.getY();
-      if (headX == x && headY < y) {
-        y--;
-      }
-      else if (headX == x && headY > y) {
-        y++;
-      }
-      else if (headY == y && headX > x) {
-        x++;
-      }
-      else if (headY == y && headX < x) {
-        x--;
-      }
-      else {
-        if (headX > x && headY > y) {
-          x++;
-          y++;
-        }
-        else if (headX > x && headY < y) {
-          x++;
+    public void move(Point p) {
+        int headX = p.getX();
+        int headY = p.getY();
+        if (headX == x && headY < y) {
           y--;
         }
-        else if (headX < x && headY > y) {
-          x--;
+        else if (headX == x && headY > y) {
           y++;
         }
-        else if (headX < x && headY < y) {
-          x--;
-          y--;
+        else if (headY == y && headX > x) {
+          x++;
         }
-      }
+        else if (headY == y && headX < x) {
+          x--;
+        }
+        else {
+          if (headX > x && headY > y) {
+            x++;
+            y++;
+          }
+          else if (headX > x && headY < y) {
+            x++;
+            y--;
+          }
+          else if (headX < x && headY > y) {
+            x--;
+            y++;
+          }
+          else if (headX < x && headY < y) {
+            x--;
+            y--;
+          }
+        }
+    }
   }
-
-  public static void main(String[] args) {
-    // Point n = new Point(-5, 3);
-    // Point x = new Point(-3, 3);
-    // System.out.println(n.toString() + " " + x.toString());
-    // System.out.println(n.touching(x));
-    // x.move(n, "L");
-    // System.out.println(n.toString() + " " + x.toString());
-  }
-}
